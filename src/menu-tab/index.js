2296066b9d5b9631e0e0bfbfb9cddbb54faf5aef
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-09 13:02:32 +0800

    Add menu page

--- src/menu-tab/index.js
+++ src/menu-tab/index.js
@@ -1,5 +1,39 @@
+import menu from '../data/menu.json';
+import './style.css';
+
 export default function MenuTab() {
   const menuTab = document.createElement('div');
+  const menuDisplay = document.createElement('div');
+
+  for (let coffee of menu) {
+    const row = document.createElement('div');
+    const coffeeNameDiv = document.createElement('div');
+    const pricesDiv = document.createElement('div');
+
+    coffeeNameDiv.textContent = coffee.name;
+
+    for (let price in coffee.prices) {
+      const priceDiv = document.createElement('div');
+      priceDiv.textContent = coffee.prices[price].toFixed(2);
+
+      pricesDiv.appendChild(priceDiv);
+    }
+
+    row.classList.add('menu-table-row');
+    coffeeNameDiv.classList.add('menu-table-row-coffee-name');
+    pricesDiv.classList.add('menu-table-row-prices');
+
+    row.append(coffeeNameDiv, pricesDiv);
+
+    menuDisplay.appendChild(row);
+  }
+
+  menuDisplay.id = 'menu-table';
+
+  menuTab.classList.add('tab-page');
+  menuTab.id = 'menu-tab';
+
+  menuTab.appendChild(menuDisplay);
 
   return menuTab;
 }
